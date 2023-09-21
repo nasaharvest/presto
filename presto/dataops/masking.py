4d823d1b94546fe8622bac77c251027425d4d4ad
Gabriel Tseng <gabriel.tseng@mail.mcgill.ca>
2023-09-21 07:36:14 -0400

    Update masking function to handle static-in-time variables

--- presto/dataops/masking.py
+++ presto/dataops/masking.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 from dataclasses import dataclass
-from random import choice, randint, sample
-from typing import Tuple
+from random import choice, randint, random, sample
+from typing import Any, List, Tuple
 
 import numpy as np
 from pandas.compat._optional import import_optional_dependency
@@ -21,15 +21,10 @@ MASK_STRATEGIES = (
     "random_combinations",
 )
 
-
 # This is to allow a quick expansion of the mask from
 # group-channel space into real-channel space
 BAND_EXPANSION = [len(x) for x in BANDS_GROUPS_IDX.values()]
-REMAPPED_INDICES = [0] * 17
-for group_index, (group, indices) in enumerate(BANDS_GROUPS_IDX.items()):
-    up_to = sum(BAND_EXPANSION[:group_index])
-    for i, idx in enumerate(indices):
-        REMAPPED_INDICES[idx] = up_to + i
+SRTM_INDEX = list(BANDS_GROUPS_IDX.keys()).index("SRTM")
 
 
 MaskedExample = namedtuple(
@@ -45,44 +40,81 @@ def make_mask(strategy: str, mask_ratio: float) -> Tuple[np.ndarray, np.ndarray]
         strategy: The masking strategy to use. One of MASK_STRATEGIES
         mask_ratio: The percentage of values to mask. Between 0 and 1.
     """
+
+    # SRTM is included here, but ignored by Presto
     mask = np.full((NUM_TIMESTEPS, len(BANDS_GROUPS_IDX)), False)
     dw_mask = np.full(NUM_TIMESTEPS, False)
-    num_tokens_masked = int((NUM_TIMESTEPS * (len(BANDS_GROUPS_IDX) + 1)) * mask_ratio)
+    srtm_mask = False
+    num_tokens_to_mask = int(((NUM_TIMESTEPS * len(BANDS_GROUPS_IDX)) + 1) * mask_ratio)
+
+    def mask_topography(srtm_mask, num_tokens_to_mask, mask_ratio):
+        should_flip = random() < mask_ratio
+        if should_flip:
+            srtm_mask = True
+            num_tokens_to_mask -= 1
+        return srtm_mask, num_tokens_to_mask
+
+    def random_masking(mask, dw_mask, num_tokens_to_mask: int):
+        if num_tokens_to_mask > 0:
+            # we set SRTM to be True - this way, it won't get randomly assigned.
+            # at the end of the function, it gets properly assigned
+            mask[:, SRTM_INDEX] = True
+            # then, we flatten the mask and dw arrays
+            all_tokens_mask = np.concatenate([dw_mask, mask.flatten()])
+            unmasked_tokens = all_tokens_mask == False
+            idx = np.flatnonzero(unmasked_tokens)
+            np.random.shuffle(idx)
+            idx = idx[:num_tokens_to_mask]
+            all_tokens_mask[idx] = True
+            mask = all_tokens_mask[NUM_TIMESTEPS:].reshape((NUM_TIMESTEPS, len(BANDS_GROUPS_IDX)))
+            dw_mask = all_tokens_mask[:NUM_TIMESTEPS]
+        return mask, dw_mask
+
     # RANDOM BANDS
-    if strategy == "group_bands":
+    if strategy == "random_combinations":
+        srtm_mask, num_tokens_to_mask = mask_topography(srtm_mask, num_tokens_to_mask, mask_ratio)
+        mask, dw_mask = random_masking(mask, dw_mask, num_tokens_to_mask)
+
+    elif strategy == "group_bands":
+        srtm_mask, num_tokens_to_mask = mask_topography(srtm_mask, num_tokens_to_mask, mask_ratio)
+        # next, we figure out how many tokens we can mask
+        num_band_groups_to_mask = int(num_tokens_to_mask / NUM_TIMESTEPS)
+        num_tokens_to_mask -= NUM_TIMESTEPS * num_band_groups_to_mask
+        assert num_tokens_to_mask >= 0
         # tuple because of mypy, which thinks lists can only hold one type
-        band_groups: Tuple = tuple(list(range(len(BANDS_GROUPS_IDX)))) + ("DW",)
-        num_band_groups_to_mask = int(num_tokens_masked / NUM_TIMESTEPS)
+        band_groups: List[Any] = list(range(len(BANDS_GROUPS_IDX))) + ["DW"]
+        band_groups.remove(SRTM_INDEX)
         band_groups_to_mask = sample(band_groups, num_band_groups_to_mask)
         for band_group in band_groups_to_mask:
             if band_group == "DW":
                 dw_mask[:] = True
             else:
                 mask[:, band_group] = True
+        mask, dw_mask = random_masking(mask, dw_mask, num_tokens_to_mask)
 
     # RANDOM TIMESTEPS
     elif strategy == "random_timesteps":
-        timesteps_to_mask = int(num_tokens_masked / (len(BANDS_GROUPS_IDX) + 1))
+        srtm_mask, num_tokens_to_mask = mask_topography(srtm_mask, num_tokens_to_mask, mask_ratio)
+        # +1 for dynamic world, -1 for the SRTM
+        timesteps_to_mask = int(num_tokens_to_mask / (len(BANDS_GROUPS_IDX)))
+        num_tokens_to_mask -= (len(BANDS_GROUPS_IDX)) * timesteps_to_mask
         timesteps = sample(TIMESTEPS_IDX, k=timesteps_to_mask)
         mask[timesteps] = True
         dw_mask[timesteps] = True
+        mask, dw_mask = random_masking(mask, dw_mask, num_tokens_to_mask)
     elif strategy == "chunk_timesteps":
-        timesteps_to_mask = int(num_tokens_masked / (len(BANDS_GROUPS_IDX) + 1))
+        srtm_mask, num_tokens_to_mask = mask_topography(srtm_mask, num_tokens_to_mask, mask_ratio)
+        timesteps_to_mask = int(num_tokens_to_mask / (len(BANDS_GROUPS_IDX)))
+        num_tokens_to_mask -= (len(BANDS_GROUPS_IDX)) * timesteps_to_mask
         start_idx = randint(0, NUM_TIMESTEPS - timesteps_to_mask)
         mask[start_idx : start_idx + timesteps_to_mask] = True  # noqa
         dw_mask[start_idx : start_idx + timesteps_to_mask] = True  # noqa
-
-    # RANDOM COMBINATIONS
-    elif strategy == "random_combinations":
-        all_tokens_mask = np.full(NUM_TIMESTEPS * (len(BANDS_GROUPS_IDX) + 1), False)
-        all_tokens_mask[:num_tokens_masked] = True
-        np.random.shuffle(all_tokens_mask)
-        dw_mask = all_tokens_mask[:NUM_TIMESTEPS]
-        mask = all_tokens_mask[NUM_TIMESTEPS:].reshape((NUM_TIMESTEPS, len(BANDS_GROUPS_IDX)))
+        mask, dw_mask = random_masking(mask, dw_mask, num_tokens_to_mask)
     else:
         raise ValueError(f"Unknown strategy {strategy} not in {MASK_STRATEGIES}")
 
-    return np.repeat(mask, BAND_EXPANSION, axis=1)[:, REMAPPED_INDICES], dw_mask
+    mask[:, SRTM_INDEX] = srtm_mask
+    return np.repeat(mask, BAND_EXPANSION, axis=1), dw_mask
 
 
 @dataclass
@@ -98,13 +130,6 @@ class MaskParams:
                 "chunk_timesteps",
                 "random_combinations",
             ]
-        num_tokens_masked = (NUM_TIMESTEPS * (len(BANDS_GROUPS_IDX) + 1)) * self.ratio
-        print(num_tokens_masked)
-        assert num_tokens_masked % 1 == 0
-        if ("random_timesteps" in self.strategies) or ("chunk_timesteps" in self.strategies):
-            assert num_tokens_masked % NUM_TIMESTEPS == 0
-        if "group_bands" in self.strategies:
-            assert num_tokens_masked % (len(BANDS_GROUPS_IDX) + 1) == 0
 
     def mask_data(self, eo_data: np.ndarray, dw_data: np.ndarray):
         strategy = choice(self.strategies)
